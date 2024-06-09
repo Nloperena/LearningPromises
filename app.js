68fe19648861c57ed7494492a15b513985879da3
Nico <nicholasloperena@gmail.com>
2024-06-09 16:48:33 -0400

    completed Promises exercise

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-function getVideo(subScriptionStatus) {
+const statusRef = document.querySelector('.status');
+// Corrected the selector here, removed the '#' prefix
+const videoRef = document.getElementById('video');
+
+function getSubscriptionStatus() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            resolve('VIP');
+        }, 2000);
+    });
+}
 
-        })
-    })
+function getVideo(subscriptionStatus) {
+    return new Promise((resolve, reject) => {
+        if (subscriptionStatus === 'VIP')
+            resolve('show video')
+        else if (subscriptionStatus === 'FREE')
+            resolve('show trailer')
+        else {
+            reject('no video')
+        }
+    });
 }
 
 async function main() {
-    const status = await getSubScriptionStatus();
+    const status = await getSubscriptionStatus();
     statusRef.innerHTML = status;
+    try {
+        const videoMessage = await getVideo(status);
+        console.log(videoMessage);
+        videoRef.innerHTML = videoMessage;
+    }
+    catch (e) {
+        console.log(e);
+        videoRef.innerHTML = e;
+    }
 }
 
 main();
